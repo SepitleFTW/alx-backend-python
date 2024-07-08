9476c71663d278b2e094fd0d55d13bb7899d1759
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-07-08 22:02:09 +0200

    fixed indent error

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -20,4 +20,4 @@ async def wait_n(n: int, max_delay:int) -> List[float]:
         *list(map(lambda _: wait_random(max_delay), range(n)))
     )
 
-        return sorted(waits)
+    return sorted(waits)
