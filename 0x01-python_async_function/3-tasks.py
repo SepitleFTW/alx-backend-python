561f5c52134369bc1703fbe9fe43a203f7495e03
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-07-08 21:53:53 +0200

    lamba is kinda hard lmao

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,3 +2,13 @@
 """
 third task
 """
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    make async task for wait_random
+    """
+    return asyncio.create_task(wait_random(max_delay))
