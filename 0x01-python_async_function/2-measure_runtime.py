676d442926d19d3caca211424adadf2ed71502e4
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-07-08 21:49:31 +0200

    fixed syntax issues

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 """
-
+measureing the runtime
 """
+
+
+import asyncio
+import time
+
+
+wait_n = __import__("1-concurrent_coroutines").wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    finds the ave runtime of wait_n
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    return (time.time() - start_time) / n
