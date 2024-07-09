38578874c61e2d00821b70ed0c59595b3cef7e09
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-07-09 23:06:08 +0200

    task2 done for reals lol

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -11,7 +11,7 @@ import typing
 async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_generator() -> typing.List[float]:
+async def async_comprehension() -> typing.List[float]:
     """
     make 10 numbers
     """
