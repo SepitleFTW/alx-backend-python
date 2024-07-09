d673600bedfcf4cabeec892a3266e9479c4531bc
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-07-09 23:05:01 +0200

    task2 done

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+task 1- Import async_generator from the previous task and then
+write a coroutine
+called async_comprehension that takes no arguments
+"""
+
+
+import typing
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_generator() -> typing.List[float]:
+    """
+    make 10 numbers
+    """
+
+    return [rand async for rand in async_generator()]
